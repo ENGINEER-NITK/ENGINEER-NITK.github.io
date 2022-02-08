95dd3883f52a7e11f8bb5b801bfb8c1181072f0c
Gaurav Chaurasia <gauravk3899@gmail.com>
2022-02-09 03:34:50 +0530

    add footer

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowPosition);
+});
