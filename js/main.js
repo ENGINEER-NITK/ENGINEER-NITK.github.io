f1872a249de560cf1284b160b7f85dd81ac67f45
Sachin Saroha <sksaroha1212@gmail.com>
2022-02-10 00:56:39 +0530

    add about page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,37 @@ window.addEventListener("scroll", function () {
   let windowPosition = window.scrollY > 0;
   header.classList.toggle("scrolling-active", windowPosition);
 });
+
+
+// js for gallery carosal
+$(".gallery-section-slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  speed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
