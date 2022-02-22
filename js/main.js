826620f7aac2c9b4ff84d7f67759b08d8bb1dcbb
Sachin Saroha <sksaroha1212@gmail.com>
2022-02-23 03:05:51 +0530

    Update links

--- js/main.js
+++ js/main.js
@@ -4,6 +4,24 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("scrolling-active", windowPosition);
 });
 
+// timer for landing page
+var countDownDate = new Date("Mar 3, 2022 00:00:00").getTime();
+var x= setInterval(function(){
+  var now =new Date().getTime();
+  var distance = countDownDate - now;
+
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML= days;
+document.getElementById("hours").innerHTML= (hours<10 ? "0" + hours : hours) + " :";
+document.getElementById("minutes").innerHTML= (minutes<10 ? "0" + minutes : minutes) + " : ";
+document.getElementById("seconds").innerHTML= (seconds<10 ? "0" + seconds : seconds);
+},1000);		
+
 
 // js for gallery carosal
 $(".gallery-section-slider").slick({
