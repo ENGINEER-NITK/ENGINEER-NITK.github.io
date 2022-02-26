c7e2e5cfbda9d8c150121665015a9cb581db1377
Sachin Saroha <sksaroha1212@gmail.com>
2022-02-26 14:53:12 +0530

    timeline update

--- js/main.js
+++ js/main.js
@@ -79,3 +79,23 @@ btn.addEventListener("click" ,()=>{
 
 // animate on scroll
 AOS.init();
+
+//timeline
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var day = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-day');
+    return '<span class="' + className + '">' + day + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
