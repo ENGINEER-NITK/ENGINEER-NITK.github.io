71e2ab70903f8d20bd75abd68e489848fd66f66a
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-09-16 18:46:11 +0530

    [Team] : Made A Team Page

---
+++ src/pages/Team/constants.js
@@ -0,0 +1,58 @@
+import { anas, mardav, shadab, shashank, hritik, preetham, abhay } from '../../assets/data/team'
+
+export const People = [{
+        name: 'ABHAY THORAT',
+        imageUrl: abhay, // Use publicly accessible image URLs
+        description: 'Convener',
+        position: 'CONVENER',
+        linkedinUrl: 'https://www.linkedin.com/in/abhayst/'
+    },
+    {
+        name: 'ANAZ AIJAS',
+        imageUrl: anas,
+        description: 'Web Team Lead',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/anasaijaz/'
+    },
+    {
+        name: 'MARDAV GANDHI',
+        imageUrl: mardav,
+        description: 'Web Developer',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/mardav-gandhi-a44103225/'
+    },
+
+    {
+        name: 'PREETHAM G',
+        imageUrl: preetham,
+        description: 'Web Developer',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/preetham-g-aa4652229/'
+    },
+
+    {
+        name: 'SHADAB',
+        imageUrl: shadab,
+        description: 'Web Developer',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/mohammed-shadab-alam-041502261/'
+    },
+
+    {
+        name: 'HRITIK SARRAF',
+        imageUrl: hritik,
+        description: 'Web Developer',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/'
+    },
+
+    {
+        name: 'SHASHANK RANJAN',
+        imageUrl: shashank,
+        description: 'Imposter',
+        position: 'WEB TEAM',
+        linkedinUrl: 'https://www.linkedin.com/in/'
+    },
+];
+
+export const Category = ['CONVENER', 'WEB TEAM'];
