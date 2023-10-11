e05762be5e55ee13e6684f3bdc5a7bc4b0196723
Preetham <preethamgr123@gmail.com>
2023-10-11 18:37:05 +0530

    links fixed

--- src/assets/data/homeCTA.js
+++ src/assets/data/homeCTA.js
@@ -1,22 +1,3 @@
-// import image1 from "./../../assets/gallery/drone2cta.jpg";
-// import image2 from "./../../assets/gallery/drone1cta.jpg";
-// import image3 from "./../../assets/gallery/drone3cta.jpg";
-// import bnr1 from "./../../assets/gallery/BNR_front.JPG";
-// import bnr2 from "./../../assets/gallery/BNR_side.JPG";
-// import bnr3 from "./../../assets/gallery/BNR_side2.jpg";
-// import drone3 from "./../../assets/gallery/drone3.jpg";
-// import proShow1 from "./../../assets/gallery/proshow1.jpg";
-// import proShow2 from "./../../assets/gallery/proshow2.jpg";
-// import proShow3 from "./../../assets/gallery/proshow3.jpg";
-// import proShow4 from "./../../assets/gallery/proshow4.jpg";
-// import proShow5 from "./../../assets/gallery/proshow5.jpg";
-// import proShow6 from "./../../assets/gallery/proshow6.JPG";
-import magic1 from "./../../assets/gallery/magic.JPG";
-// import magic2 from "./../../assets/gallery/magic2.JPG";
-// // import Person1 from "./../../assets/gallery/Person1.CR2";
-
-
-
 export const imageGallery1 = [
     {
         url: "https://s6.imgcdn.dev/ZHwWy.jpg",
@@ -42,7 +23,6 @@ export const imageGallery1 = [
         url: "https://s6.imgcdn.dev/ZHsWd.jpg",
         caption: "Majestic Mountains",
     },
-
     // ... add more images
 ];
 
@@ -76,5 +56,5 @@ export const imageGallery2 = [
         url: "    https://s6.imgcdn.dev/ZH4EH.jpg",
         caption: "Majestic Mountains",
     },
-
+    // ... add more images
 ]
